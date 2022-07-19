a3f5297752150c30251e2a2782bf00d597b53dcc
Yosua Nicolaus <cryodawn257@gmail.com>
2022-07-19 14:46:38 +0700

    create utils file, convertUci to index function

---
+++ utils.js
@@ -0,0 +1,12 @@
+function convertUciLocation(uci) {
+  const fileIndex = uci[0].charCodeAt() - 97;
+  const rankIndex = Number(uci[1]) + (4 - Number(uci[1])) * 2;
+
+  if (fileIndex < 0 || fileIndex > 7) {
+    throw "file is out of bonds";
+  } else if (rankIndex < 0 || rankIndex > 7) {
+    throw "rank is out of bonds";
+  }
+
+  return [fileIndex, rankIndex];
+}
